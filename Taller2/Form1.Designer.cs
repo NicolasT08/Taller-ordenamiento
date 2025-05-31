2337c6dfd22f5f4e1a735e1ab957c276601b4906
Nicolas Tinjaca <nicolas.tinjaca@uptc.edu.co>
2025-05-31 09:14:15 -0500

    (Add) sorting

---
+++ Taller2/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Taller2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart6 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "30";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cantidad Total";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(85, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(205, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Minimo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Maximo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(574, 41);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(276, 200);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "Aleatorios";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(856, 41);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(272, 200);
+            this.chart2.TabIndex = 15;
+            this.chart2.Text = "slightlyasc";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(1134, 41);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(284, 200);
+            this.chart3.TabIndex = 16;
+            this.chart3.Text = "slightdesc";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Location = new System.Drawing.Point(574, 247);
+            this.chart4.Name = "chart4";
+            series4.ChartArea = "ChartArea1";
+            series4.Name = "Series1";
+            this.chart4.Series.Add(series4);
+            this.chart4.Size = new System.Drawing.Size(276, 200);
+            this.chart4.TabIndex = 17;
+            this.chart4.Text = "orderer";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 243);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(347, 419);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Funcion";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 70;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Duracion";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 75;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Tiempo";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 67;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Cantidad";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 74;
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            this.chart5.Location = new System.Drawing.Point(856, 247);
+            this.chart5.Name = "chart5";
+            series5.ChartArea = "ChartArea1";
+            series5.Name = "Series1";
+            this.chart5.Series.Add(series5);
+            this.chart5.Size = new System.Drawing.Size(272, 200);
+            this.chart5.TabIndex = 21;
+            this.chart5.Text = "heapsort";
+            // 
+            // chart6
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart6.ChartAreas.Add(chartArea6);
+            this.chart6.Location = new System.Drawing.Point(1134, 247);
+            this.chart6.Name = "chart6";
+            series6.ChartArea = "ChartArea1";
+            series6.Name = "Series1";
+            this.chart6.Series.Add(series6);
+            this.chart6.Size = new System.Drawing.Size(284, 200);
+            this.chart6.TabIndex = 22;
+            this.chart6.Text = "bubblesort";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1442, 674);
+            this.Controls.Add(this.chart6);
+            this.Controls.Add(this.chart5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart6;
+    }
+}
+
---
+++ Taller2/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller2
+{
+    public partial class Form1 : Form
+    {
+        int size;
+        int limitDown, limitUp;
+        Stopwatch stopwatch;
+        List<int> allRandoms = new List<int>();
+        List<int> slightlyAscend = new List<int>();
+        List<int> slightlyDescend = new List<int>();
+        List<int> orderer = new List<int>();
+        List<int> dataTime = new List<int>();
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clearCharts();
+
+            size = Convert.ToInt32(textBox1.Text);
+            limitDown = Convert.ToInt32(textBox2.Text);
+            limitUp = Convert.ToInt32(textBox3.Text);
+            
+            generateData();
+            sorts();
+
+            string[] etiquetas = new string[]
+            {
+                "random",
+                "levemente Asc",
+                "levemente desc",
+                "ordenado",
+                "HeapSort",
+                "bubblesort"
+            };
+
+            for (int i = 0; i < dataTime.Count && i < etiquetas.Length; i++)
+            {
+                dataGridView1.Rows.Add(
+                    etiquetas[i] + i,
+                    stopwatch.Elapsed.Seconds.ToString() + ": " + stopwatch.Elapsed.Milliseconds.ToString(),
+                    stopwatch.ElapsedMilliseconds.ToString(),
+                    size.ToString()
+                );
+            }
+        }
+
+        void generateData()
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //aleatorio
+            for (int i = 0; i < size; i++)
+            {
+                int num = random.Next(limitDown, limitUp + 1);
+                allRandoms.Add(num);
+                chart1.Series[0].Points.Add(num);
+            }
+
+            stopwatch.Stop();
+            dataTime.Add(stopwatch.Elapsed.Seconds);
+
+
+            //ordanado levemente ascendentemente
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            slightlyAscend = new List<int>();
+            int blockSize = size / 5;
+            for (int i = 0; i < 5; i++)
+            {
+                int min = limitDown + i * (limitUp - limitDown) / 5;
+                int max = limitDown + (i + 1) * (limitUp - limitDown) / 5;
+                List<int> block = new List<int>();
+                for (int j = 0; j < blockSize; j++)
+                    block.Add(random.Next(min, max + 1));
+
+                block.Sort();
+                slightlyAscend.AddRange(block);
+            }
+            stopwatch.Stop();
+            dataTime.Add(stopwatch.Elapsed.Seconds);
+            foreach (var val in slightlyAscend)
+                chart2.Series[0].Points.Add(val);
+
+
+            //ordanado levemente descendentemente
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            slightlyDescend = new List<int>();
+            for (int i = 4; i >= 0; i--)
+            {
+                int min = limitDown + i * (limitUp - limitDown) / 5;
+                int max = limitDown + (i + 1) * (limitUp - limitDown) / 5;
+                List<int> block = new List<int>();
+                for (int j = 0; j < blockSize; j++)
+                    block.Add(random.Next(min, max + 1));
+
+                block.Sort((a, b) => b.CompareTo(a));
+                slightlyDescend.AddRange(block);
+            }
+            stopwatch.Stop();
+            dataTime.Add(stopwatch.Elapsed.Seconds);
+            foreach (var val in slightlyDescend)
+                chart3.Series[0].Points.Add(val);
+
+
+            //ordenado
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            orderer = new List<int>(allRandoms);
+            orderer.Sort();
+            stopwatch.Stop();
+            dataTime.Add(stopwatch.Elapsed.Seconds);
+            foreach (var val in orderer)
+                chart4.Series[0].Points.Add(val);
+        }
+
+        void sorts()
+        {
+            int[] dataForHeap = (int[])allRandoms.ToArray().Clone();
+            int[] dataForBubble = (int[])allRandoms.ToArray().Clone();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            HeapSort(dataForHeap);
+
+            stopwatch.Stop();
+            dataTime.Add(stopwatch.Elapsed.Seconds);
+            foreach (var val in dataForHeap)
+                chart5.Series[0].Points.Add(val);
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var resultBubble = BubbleSort(dataForBubble);
+
+            stopwatch.Stop();
+            dataTime.Add(stopwatch.Elapsed.Seconds);
+            foreach (var val in resultBubble)
+                chart6.Series[0].Points.Add(val);
+        }
+
+        void clearCharts()
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            chart3.Series[0].Points.Clear();
+            chart4.Series[0].Points.Clear();
+            chart5.Series[0].Points.Clear();
+            chart6.Series[0].Points.Clear();
+            allRandoms.Clear();
+            slightlyAscend.Clear();
+            slightlyDescend.Clear();
+        }
+
+        int[] BubbleSort(int[] inputArray)
+        {
+            int n = inputArray.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (inputArray[j] < inputArray[j + 1])
+                    {
+                        int temp = inputArray[j];
+                        inputArray[j] = inputArray[j + 1];
+                        inputArray[j + 1] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+
+        public void HeapSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
+        }
+
+        private void Heapify(int[] arr, int n, int i)
+        {
+            int smallest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && arr[left] < arr[smallest])
+                smallest = left;
+
+            if (right < n && arr[right] < arr[smallest])
+                smallest = right;
+
+            if (smallest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[smallest];
+                arr[smallest] = swap;
+
+                Heapify(arr, n, smallest);
+            }
+        }
+
+    }
+}
